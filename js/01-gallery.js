5f2f6e56e198c13b864122bfdf50165b4605adfc
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-04-23 00:30:36 +0100

    fulfilled

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const ul = document.querySelector('.gallery');
+
+for (const galleryItem of galleryItems) {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  const img = document.createElement('img');
+
+  li.classList.add('gallery__item');
+  a.classList.add('gallery__link');
+  img.classList.add('gallery__image');
+
+  img.setAttribute('src', galleryItem.preview);
+  img.setAttribute('data-source', galleryItem.original);
+  img.setAttribute('alt', galleryItem.description);
+  a.setAttribute('href', galleryItem.original);
+  
+    a.addEventListener('click', (event) => {
+        event.preventDefault();
+        const instance = basicLightbox.create(`
+        <img src="${galleryItem.original}" alt="${galleryItem.description}">
+        `);
+        instance.show();
+    });
+
+  a.appendChild(img);
+  li.appendChild(a);
+  ul.appendChild(li);
+}
+
+
