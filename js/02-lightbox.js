de119b823920c4fdf32bcca85f18a9f27d54c8dc
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-04-23 14:52:49 +0100

    step 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const ul = document.querySelector('.gallery')
+
+for (const galleryItem of galleryItems) {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    const img = document.createElement('img')
+    
+    li.classList.add('gallery__item')
+    a.classList.add('gallery__link')
+    img.classList.add('gallery__image')
+
+    a.setAttribute('href', galleryItem.original)
+    img.setAttribute('src', galleryItem.preview)
+    img.setAttribute('alt', galleryItem.description)
+
+    a.appendChild(img)
+    li.appendChild(a)
+    ul.appendChild(li)
+}
 
 console.log(galleryItems);
